2527ec41e6e592a01b73b556f5dfc9c4eccb0c2e
Nizar El Jurdi <nizareljurdi@gmail.com>
2023-05-15 19:54:18 -0400

    fix base dir

--- backend/blabrm0uth/blabrm0uth/settings.py
+++ backend/blabrm0uth/blabrm0uth/settings.py
@@ -17,11 +17,11 @@ from decouple import config
 API_KEY = config('API_KEY')
 SECRET_KEY = config('SECRET_KEY')
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
